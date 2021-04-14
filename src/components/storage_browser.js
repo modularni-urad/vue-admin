1451e0dbd266b07e6f8b106737ed7ded0e9fcf07
vencax <vencax@centrum.cz>
2021-04-14 13:27:04 +0200

    missing file

---
+++ src/components/storage_browser.js
@@ -0,0 +1,17 @@
+export default {
+  data: () => {
+    return {
+      found: [],
+      loading: true
+    }
+  },
+  props: ['url', 'data', 'onSelect'],
+  methods: {
+
+  },
+  template: `
+  <div>
+    HOVNO
+  </div>
+  `
+}
