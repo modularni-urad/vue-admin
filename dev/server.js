aed34f487a385b527d14abee5d09afc0d0246e05
vencax <vencax@centrum.cz>
2021-09-19 14:40:58 +0200

    api proxy

--- dev/server.js
+++ dev/server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 // const express = require('express')
+const HttpProxy = require('http-proxy-middleware')
 const BS = require('browser-sync')
 const bs = BS.create()
 const SRC_DIR = path.resolve(path.join(__dirname, '../src'))
@@ -9,7 +10,14 @@ bs.init({
   server: [ SRC_DIR, DEV_DIR ],
   port: 8080,
   open: false,
-  ui: false
+  ui: false,
+  middleware: [{
+    route: '/api',
+    handle: HttpProxy.createProxyMiddleware({ 
+      target: 'https://stredni.web.otevrenamesta.cz/', 
+      changeOrigin: true 
+    })
+  }]
 })
 bs.watch(DEV_DIR + '/index.html').on('change', bs.reload)
 bs.watch(SRC_DIR + '/**/*.js').on('change', function (filepath, file) {
