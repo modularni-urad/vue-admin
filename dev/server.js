220abd370a782729eee383d3cc67c6a8def12def
vencax <vencax@centrum.cz>
2021-11-06 09:08:52 +0100

    debug parts commented

--- dev/server.js
+++ dev/server.js
@@ -6,6 +6,18 @@ const bs = BS.create()
 const SRC_DIR = path.resolve(path.join(__dirname, '../src'))
 const DEV_DIR = path.resolve(__dirname)
 
+// const token = null
+// function onProxyReq(proxyReq, req, res) {
+//   console.log(proxyReq)
+// }
+// function onProxyRes(proxyRes, req, res) {
+//   // if (proxyRes.headers['set-cookie']) {
+//   //   const m = proxyRes.headers['set-cookie'].match(/Bearer=([^;]*);*.$/)
+//   //   token = m[0]
+//   // }
+//   console.log(proxyRes)
+// }
+
 bs.init({
   server: [ SRC_DIR, DEV_DIR ],
   port: 8080,
@@ -15,7 +27,8 @@ bs.init({
     route: '/api',
     handle: HttpProxy.createProxyMiddleware({ 
       target: 'https://stredni.web.otevrenamesta.cz/', 
-      changeOrigin: true 
+      changeOrigin: true
+      // onProxyReq, onProxyRes
     })
   }]
 })
