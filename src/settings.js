61d18b6640b62822321b24a19c4536946282ee8e
vencax <vencax@centrum.cz>
2022-03-16 15:35:35 +0100

    json settings loadint

--- src/settings.js
+++ src/settings.js
@@ -8,7 +8,9 @@ function replaceConst (constsLeft, consts, settings) {
 
 export default async function loadSettings (url) {
   const req = await axios(url)
-  const s = jsyaml.load(req.data)
+  const s = url.match(/.*yaml$/)
+    ? jsyaml.load(req.data)
+    : JSON.parse(req.data)
   return s.consts && Object.keys(s.consts).length > 0
     ? jsyaml.load(replaceConst(Object.keys(s.consts), s.consts, req.data))
     : s
