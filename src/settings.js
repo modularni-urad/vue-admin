8a83efc9ac4ada81b819ee862ed33027491da800
vencax <vencax@centrum.cz>
2022-03-16 15:43:51 +0100

    fix

--- src/settings.js
+++ src/settings.js
@@ -9,7 +9,8 @@ function replaceConst (constsLeft, consts, settings) {
 export default async function loadSettings (url) {
   const req = await axios(url)
   const s = url.match(/.*yaml$/) ? jsyaml.load(req.data) : req.data
+  const rawData = url.match(/.*yaml$/) ? req.data : jsyaml.dump(req.data)
   return s.consts && Object.keys(s.consts).length > 0
-    ? jsyaml.load(replaceConst(Object.keys(s.consts), s.consts, req.data))
+    ? jsyaml.load(replaceConst(Object.keys(s.consts), s.consts, rawData))
     : s
 }
